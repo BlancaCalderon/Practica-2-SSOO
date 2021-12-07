afa4ccefacdcedd8c2787f098a3bd78c0852ec46
Blanca Calderón <79267061+BlancaCalderon@users.noreply.github.com>
2021-12-07 17:08:11 +0100

    Update minishell.c
    fase 3 y fase 5

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -11,13 +11,29 @@
 
 #include "internas.h"
 #include "entrada_minishell.h"
-//#include "ejecutar.h"
+#include "ejecutar.h"
 
+static void manejar_sigchild(int signo) {	//función gestor de la señal
+	int estado;
+	waitpid(-1,&estado, WNOHANG);	/*como pid=-1 espera la finalización  de otro proceso y WNOHANG indica que no se suspende proceso si proceso hijo especificado por pid no se encuentra disponible, estado 					guarda el estado del proceso(esto evita que proceso hijo quede en estado zombie)*/
+
+}
 
 int main(int argc, char *argv[])
 {
 
    char buf[BUFSIZ];
+   const char s[2]=";";		//caracter que determina separacion
+   char *token;
+   struct sigaction sa;		//Define estructura sigaction
+
+   memset(&sa,0,sizeof(sa));	//sustituye tantos caracteres como tamaño de la estructura sigaction por 0 en la estructura sigaction sa
+	
+   sa.sa_handler=manejar_sigchild;	//indica que gestor de señal será la función manejar_sigchild;
+   sa.sa_flags=SA_NOCLDSTOP | SA_RESTART;	/*establece flags, SA_NOCLDSTOP indica que si el número de señal es SIGCHILD no se genera señal cuando proceso hijo se detiene y SA_RESTART que no se 							reestablece la acción de la señal*/
+   
+   sigaction(SIGCHLD, &sa, NULL);	/*número de señal es SIGCHLD(por lo que cuando se detenga el proceso hijo no se genera señal), la estructura con la que se gestionará la señal es sa y no hay 						estructura que gestionaba anteriormenete la función*/
+   
 
 
    while (1)
@@ -41,7 +57,12 @@ int main(int argc, char *argv[])
       
       else //si es orden externa
       {
-         printf("%s: Orden externa.\n", buf);
+	 token=strtok(buf,s);		//separa primer token
+	 while(token!=NULL) {		//pasa por el resto de tokens hasta terminar con la orden
+		ejecutar_linea_ordenes(token);		//llama a función con cada orden separada
+		token=strtok(NULL,s);			//siguiente token
+		printf("%s: Orden externa.\n", token);
+	}
       }
 	   
 	   
