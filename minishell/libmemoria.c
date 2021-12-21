080a24b306cb022def0a764859f90869826e3723
Blanca Calderón <79267061+BlancaCalderon@users.noreply.github.com>
2021-12-21 17:13:07 +0100

    Update libmemoria.c
    comentarios

--- minishell/libmemoria.c
+++ minishell/libmemoria.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
-
 #include "libmemoria.h"
 
-
+//Función para liberar espacio de memoria de args
 void free_argumentos(char **args)
 {
    int i = 0;
 
-   while(args[i])
-      free(args[i++]);
-   free(args);
+   while(args[i])          //Mientras existe argumento
+      free(args[i++]);     //Libera zona de memoria que ocupa siguiente argumento
+   free(args);             //Libera espacio de memoria qeu ocupa el argumento
 }
 
+//Función para liberar la zona de memoria de las ordenes introducidas usando tubería
 void free_ordenes_pipes(char **ordenes, int **fds, int nordenes)
 {
    int i = 0;
 
-   for (i = 0; i < nordenes; i++)
+   for (i = 0; i < nordenes; i++)   //Mientras i sea menor que el número de órdenes introducidas
    {
-      free(ordenes[i]);
-      if (i < (nordenes - 1))
-         free(fds[i]);
+      free(ordenes[i]);             //Libera espacio de memoria de la orden i
+      if (i < (nordenes - 1))       //Si es menor que el número de órdenes introducidas menos 1
+         free(fds[i]);              //Libera zona de memoria de fds i
    }
 
-   free(ordenes);
-   free(fds);
+   free(ordenes);                   //Libera espacio de memoria de las ordenes
+   free(fds);                       //Libera zona de memoria de fds
 }
